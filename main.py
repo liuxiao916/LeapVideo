043c1987d12a9d1e1b61f2642dc5fc250ba9b1df
XPS Ubuntu <971177213@qq.com>
2023-02-21 13:47:16 +0800

    add UI

--- main.py
+++ main.py
@@ -5,9 +5,42 @@ import sys
 import vlc
 import threading
 import time
+from PyQt5.QtWidgets import QGridLayout, QApplication, QDialog, QLineEdit, QPushButton, QFileDialog
 
 state = detection.State()
 threadLock = threading.Lock()
+
+class Dialog(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        grid = QGridLayout()
+        self.setWindowTitle("隔空手势交互系统")
+        # 第一行, 打开文件
+        self.qlineEdit_selectfile = QLineEdit('文件路径')
+        self.qpush_selectfile = QPushButton('选择视频')
+        grid.addWidget(self.qlineEdit_selectfile, 0, 1)
+        grid.addWidget(self.qpush_selectfile, 0, 2)
+        # 第三行, 按钮, 打开文件
+        self.qpush_openfile = QPushButton('开始播放')
+        grid.addWidget(self.qpush_openfile, 1, 2)
+        self.setLayout(grid)
+        # 绑定按钮
+        self.qpush_selectfile.clicked.connect(self.select_file)
+        self.qpush_openfile.clicked.connect(self.open_file)
+    def select_file(self):
+        """选择文件
+        """
+        filename, _ = QFileDialog.getOpenFileName(self, "getOpenFileName",
+                                                         './',  # 文件的起始路径
+                                                         "All Files (*);;video Files (*.mp4)") # 设置文件类型
+        self.qlineEdit_selectfile.setText(filename)
+
+    def open_file(self):
+        """使用系统的应用打开
+        """
+        play(self.qlineEdit_selectfile.text())
+
+
 
 class SampleListener(Leap.Listener):
     def on_connect(self, controller):
@@ -36,14 +69,14 @@ class SampleListener(Leap.Listener):
         # print ("Frame available")
 
 
-def main():
+def play(videopath):
     listener = SampleListener()
     controller = Leap.Controller()
     controller.set_policy(Leap.Controller.POLICY_BACKGROUND_FRAMES)
     controller.add_listener(listener)
 
     videoplayer = player.Player()
-    videoplayer.play("video/test1.mp4")
+    videoplayer.play(videopath)
     play = 'pic/play.png'
     pause = 'pic/pause.png'
     fc = 'pic/fullscreen.png'
@@ -55,7 +88,7 @@ def main():
     videoplayer.init_logo()
     videoplayer.set_marquee()
     # videoplayer.update_text("%Y-%m-%d %H:%M:%S")
-
+    
     while True:
         if state.state == 1:
             if videoplayer.is_playing():
@@ -189,4 +222,8 @@ def main():
     #     pass
 
 if __name__ == "__main__":
-    main()
+    #main()
+    app = QApplication(sys.argv)
+    dlg = Dialog()
+    dlg.show()
+    sys.exit(app.exec_())
